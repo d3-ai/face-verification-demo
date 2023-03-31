a0515c8886d5110dc7a8e02536b3971e33191fe3
Yusuke-Yamasaki <yamasaki@hal.ipc.i.u-tokyo.ac.jp>
2023-03-31 12:12:47 +0900

    feat: shell script for launch container on RaspberryPi

--- shell/pi_launch_container.sh
+++ shell/pi_launch_container.sh
@@ -1,11 +1,10 @@
 docker run -it --rm\
-  -u $UID:$GID \
   --name test \
   --mount type=bind,source="$(pwd)"/shell/,target=/home/"${USER}"/project/shell/,readonly \
   --mount type=bind,source="$(pwd)"/data/,target=/home/"${USER}"/project/data/,readonly \
-  --mount type=bind,source="$(pwd)"/local/,target=/home/"${USER}"/project/local/,readonly \
   --mount type=bind,source="$(pwd)"/models/,target=/home/"${USER}"/project/models/,readonly \
-  --mount type=bind,source="$(pwd)"/face_verification/,target=/home/"${USER}"/project/face_verification/,readonly \
   --mount type=bind,source="$(pwd)"/src/,target=/home/"${USER}"/project/src/,readonly \
+  --mount type=bind,source="$(pwd)"/tmp/,target=/home/"${USER}"/project/tmp/ \
   --env CID="$1" \
-  flower_mockup_pi4:latest
+  flower-face_verification_image4pi:latest \
+  /bin/bash
